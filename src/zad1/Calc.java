7d0da2cd703540583f1f8a7d14ce30814cdec2ef
rochmisz <109878606+rochmisz@users.noreply.github.com>
2024-01-12 22:30:53 +0100

    Initial commit

---
+++ src/zad1/Calc.java
@@ -0,0 +1,11 @@
+/**
+ *
+ *  @author Lipiński Bartłomiej S27645
+ *
+ */
+
+package zad1;
+
+
+public class Calc {
+}  
---
+++ src/zad2/Purchase.java
@@ -0,0 +1,11 @@
+/**
+ *
+ *  @author Lipiński Bartłomiej S27645
+ *
+ */
+
+package zad2;
+
+
+public class Purchase {
+}  
